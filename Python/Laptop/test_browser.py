be4afe2024b65457f102bba6d841876dfa21fb98
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2024-12-09 11:20:34 +0100

    Update test_browser.py

--- Python/Laptop/test_browser.py
+++ Python/Laptop/test_browser.py
@@ -6,10 +6,14 @@ BASE_PATH = "/home/michael/Smart-Home-Escape-Room-Workshop/Python/Laptop/"
 if __name__ == "__main__":
     # Verwendung: python3 automate_escape_room_laptop.py <open/close> <filetype> <room> <filename>
     # open/close
+    print("open posen")
     subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "open", "HTML", "IoT", "ErkennungPosen"])
     time.sleep(5)
+    print("close posen")
     subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "close", "HTML", "IoT", "ErkennungPosen"])
     time.sleep(5)
+    print("open skull")
     subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "open", "HTML", "IoT", "ErkennungTotenschaedel"])
     time.sleep(5)
+    print("close skull")
     subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "close", "HTML", "IoT", "ErkennungTotenschaedel"])
