f6fe39de646f880ed2638e5201c3ddc4310f6386
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2024-12-09 11:17:47 +0100

    Create test_browser.py

---
+++ Python/Laptop/test_browser.py
@@ -0,0 +1,15 @@
+import subprocess
+import time
+
+BASE_PATH = "/home/michael/Smart-Home-Escape-Room-Workshop/Python/Laptop/"
+
+if __name__ == "__main__":
+    # Verwendung: python3 automate_escape_room_laptop.py <open/close> <filetype> <room> <filename>
+    # open/close
+    subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "open", "HTML", "IoT", "ErkennungPosen"])
+    time.sleep(5)
+    subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "close", "HTML", "IoT", "ErkennungPosen"])
+    time.sleep(5)
+    subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "open", "HTML", "IoT", "ErkennungTotenschaedel"])
+    time.sleep(5)
+    subprocess.Popen(['python3', BASE_PATH + "automate_escape_room_laptop.py", "close", "HTML", "IoT", "ErkennungTotenschaedel"])
