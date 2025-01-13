95f01eb2848bf15ceb4098239f999a58890bcc75
Michael Dörflinger <michaeldoerflinger93@gmail.com>
2025-01-13 13:25:54 +0100

    Update start_node_red.sh

--- start_node_red.sh
+++ start_node_red.sh
@@ -5,7 +5,7 @@ port=8080
 nc -z -w 2 "$host" "$port"
 
 if [ $? -eq 0 ]; then
-    /home/$USER/.nvm/versions/node/v16.13.2/bin/node-red /home/michael/Smart-Home-Escape-Room-Workshop/EscapeRoom/Technik/flows.json
+    /home/$USER/.nvm/versions/node/v16.13.2/bin/node-red /home/$USER/Smart-Home-Escape-Room-Workshop/EscapeRoom/Technik/flows.json
 else
     echo "Die Adresse ist nicht erreichbar."
 fi
